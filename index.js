9b6ac1e9814f2378f356e2400cf4c5b7b6afcd03
monthergholam@gmail.com <monthergholam@gmail.com>
2018-06-25 23:01:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+//first
 function shout (string){
 
   return string.toUpperCase()
+}
+
+//Second
+function whisper (string){
+
+return string.toLowerCase()
+}
+
+//Third
+function logShout(string){
+  console.log(string.toUpperCase)
 }
